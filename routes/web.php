c137a0095c12b984f6eddca38032e60b19ad61ab
Fabio Ortiz <fabio.ortiz@tecsup.edu.pe>
2024-11-11 18:34:49 -0500

    Add PostController with routes for posts

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts', [PostController::class, 'store']);
