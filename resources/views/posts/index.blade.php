dc850934796149f5e0ce71a0af72f4f311522d5c
Fabio Ortiz <fabio.ortiz@tecsup.edu.pe>
2024-11-12 10:03:58 -0500

    Se agregaron vistas a index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,7 @@
 <!-- resources/views/posts/index.blade.php -->
-<h1>Lista de Posts</h1>
+<h1 style="color: blue; font-family: Arial, sans-serif;">Lista de Posts</h1>
 <ul>
     @foreach($posts as $post)
-        <li>{{ $post->title }}</li>
+        <li><strong>{{ $post->title }}</strong>: {{ $post->content }}</li>
     @endforeach
 </ul>
