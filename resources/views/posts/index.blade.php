a5a2abe382c46021f08b6b8eaa7e1e8dee7153ed
Fabio Ortiz <fabio.ortiz@tecsup.edu.pe>
2024-11-12 10:06:07 -0500

    Se actualizaron los cambios en la rama principal

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,7 @@
 <!-- resources/views/posts/index.blade.php -->
-<h1>Lista de Posts</h1>
+<h1 style="color: green; text-align: center;">Lista de Posts</h1>
 <ul>
     @foreach($posts as $post)
-        <li>{{ $post->title }}</li>
+        <li style="font-style: italic;"><strong>{{ $post->title }}</strong>: {{ $post->content }}</li>
     @endforeach
 </ul>
